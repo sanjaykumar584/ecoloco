18b0fe4deaa96d3dd1ba1551ad8839f13d36342b
sanjaykumar584 <sanjay050804@gmail.com>
2024-11-22 00:39:47 +0530

    auth and profile page

--- ecoloco/src/App.jsx
+++ ecoloco/src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Enquiry from './components/Enquiry';
 import Login from './components/Login'
 import Signup from './components/Signup';
 
@@ -9,6 +12,9 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Services" element={<Services />} />
+        <Route path="/Enquiry" element={<Enquiry />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
--- ecoloco/src/components/Home.jsx
+++ ecoloco/src/components/Home.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-
+import NavBar from './NavBar'
+import Footer from './Footer'
+import Enquiry from './Enquiry'
+import Services from './Services'
 function Home() {
   return (
     <p className='text-9xl'>Home</p>
